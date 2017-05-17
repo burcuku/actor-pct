42ee28664828b7b174ddcf36ba0bb78c595a12c0
Filip Niksic <fniksic@mpi-sws.org>
2017-05-17 18:38:53 +0200

    Simplified the Bogart-Magagnosc chain decomposition

--- misc/bm.py
+++ misc/bm.py
@@ -6,9 +6,7 @@ class Element:
     def __init__(self, elem_id, preds):
         self.elem_id = elem_id
         self.preds = preds
-        self.succs = []
         self.chain_next = None
-        self.chain_prev = None
         self.chain = None
 
     def __repr__(self):
@@ -17,22 +15,21 @@ class Element:
     def __str__(self):
         return self.elem_id
 
-    def add_succ(self, elem):
-        self.succs.append(elem)
-
     def before(self, other):
-        return other in self.all_after()
+        """Returns True if the current element is before other in the poset."""
+        return self in other.all_preds()
 
-    def all_after(self):
+    def all_preds(self):
+        """Returns a generator over all (including transitive) predecessors."""
         visited = set()
-        queue = self.succs.copy()
+        queue = self.preds.copy()
         while queue:
             elem = queue[0]
             queue = queue[1:]
             if elem in visited:
                 continue
             visited.add(elem)
-            queue += elem.succs
+            queue += elem.preds
             yield elem
 
 
@@ -51,7 +48,6 @@ class Chain:
             self.head = elem
         else:
             self.tail.chain_next = elem
-            elem.chain_prev = self.tail
         self.tail = elem
         elem.chain = self
 
@@ -70,52 +66,40 @@ class ChainDecomposition:
 
     def rewire(self, a, b):
         # Rewire the elements
-        bb = a.chain_next
         a.chain_next = b
-        b.chain_prev = a
 
         # Rewire the chains
-        chain_a = a.chain
-        chain_b = b.chain
-        if bb is None:
-            chain_a.tail = chain_b.tail
-            chain_b.head = chain_b.tail = None
-        else:
-            chain_b.head = bb
-            t = chain_b.tail
-            chain_b.tail = chain_a.tail
-            chain_a.tail = t
-
-            # No need to set chain for other nodes in the new chain_b
-            bb.chain = chain_b
+        a.chain.tail = b.chain.tail
         current = b
         while current is not None:
-            current.chain = chain_a
+            current.chain = a.chain
             current = current.chain_next
-        return bb
 
-    def reduce(self, b, prev):
-        self.chains.discard(b.chain)
-        while b is not None:
-            a = prev[b]
-            b = self.rewire(a, b)
+    def reduce(self, a, b, pairs):
+        while True:
+            b_chain = b.chain
+            self.rewire(a, b)
+            if b not in pairs:
+                break
+            a, b = pairs[b]
+        self.chains.discard(b_chain)
 
     def merge(self):
-        current_q = [chain.tail for chain in self.chains]
+        current_q = [chain.head for chain in self.chains]
         next_q = []
-        prev = {}
+        pairs = {}
         while current_q:
-            for a in current_q:
-                for b in a.all_after():
-                    if b in prev:
-                        continue
-                    prev[b] = a
-                    if b.chain_prev is None:
-                        # We have found a reducing sequence
-                        self.reduce(b, prev)
+            for b in current_q:
+                for a in b.all_preds():
+                    a_next = a.chain_next
+                    if a_next is None:
+                        # We have found a reducing sequence starting with a
+                        self.reduce(a, b, pairs)
                         return True
-                    else:
-                        next_q.append(b.chain_prev)
+                    if a_next in pairs:
+                        continue
+                    pairs[a_next] = (a, b)
+                    next_q.append(a_next)
             current_q = next_q
             next_q = []
         return False
@@ -127,6 +111,7 @@ class ChainDecomposition:
                 print(elem.elem_id, end=' ')
                 elem = elem.chain_next
             print()
+
 
 class PosetReader:
     """Reads and constructs a poset from stdin"""
@@ -145,9 +130,7 @@ class PosetReader:
         ids = line.split()
         preds = [self.elems[elem_id] for elem_id in ids[1:]]
         elem = Element(ids[0], preds)
-        for pred in preds:
-            pred.add_succ(elem)
-        self.elems[ids[0]] = elem
+        self.elems[elem.elem_id] = elem
         return elem
 
     def print_decomposition(self):
